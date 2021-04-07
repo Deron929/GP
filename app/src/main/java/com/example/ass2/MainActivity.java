e04654f74a6c474079324e71d5b176d4f152f163
deron929 <leo1182038335@gmail.com>
2021-04-07 17:19:48 +0800

    hi

---
+++ app/src/main/java/com/example/ass2/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ass2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+private 1,2,3 ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
